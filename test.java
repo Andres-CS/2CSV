6def7e70890b018f34d4f4f98ad4a1966b033ff4
Jairo A Perez <jairo.perez@japc.me>
2019-06-14 04:19:30 -0400

    Java file has been added. So far is for testing.

---
+++ test.java
@@ -0,0 +1,9 @@
+public class java{
+    public static void main(String[] args){
+        System.out.println("Program written in Java");
+
+        System.out.print("Anothe line\n");
+
+        System.out.println("Concat"+"enation");
+    }
+}
